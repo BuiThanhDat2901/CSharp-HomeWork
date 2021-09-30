cd0096dd66743674baf793ec8311b30412b92f1f
Bui Thanh Dat <datbtth2011018@fpt.edu.vn>
2021-09-30 16:20:50 +0700

    lab2-3

---
+++ Lab3_ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab3_Part4_Ex2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var employee = new Employee("Bui", "Dat", "Hung Yen", 20, 100000);
+            Console.WriteLine(employee.ToString());
+            Console.WriteLine("Bonus: " + employee.Bonus(0.2));
+        }
+    }
+}
